3b509e59d402f2f9ad5b64426c9c647dc7af75cb
ivan_masiuk <vavolan@gmail.com>
2023-02-01 22:14:32 +0200

    impl: add documentation

--- server/api/ingredient.py
+++ server/api/ingredient.py
@@ -12,4 +12,22 @@ router = APIRouter(
 
 @router.get("/")
 def get_ingredients(key_word: str):
+    """
+    Returns list of found ingredients, for example:
+    `[
+      {
+        "id": 9003,
+        "name": "apple",
+        "image": "https://spoonacular.com/cdn/ingredients_500x500/apple.jpg"
+      },
+      {
+        "id": 9019,
+        "name": "applesauce",
+        "image": "https://spoonacular.com/cdn/ingredients_500x500/applesauce.png"
+      },
+    ]`
+    \f
+    :param key_word: Key ingredient word
+    :return: List found ingredients
+    """
     return get_list_ingredients(key_word)
