c51ca33a70568638ebaa99fa690cf4dc127c947d
Penkov Bohdan <webbogdan11@gmail.com>
2023-02-01 23:22:53 +0200

    For review

--- server/api/ingredient.py
+++ server/api/ingredient.py
@@ -2,7 +2,8 @@
 
 from fastapi import APIRouter
 
-from ..spoonacular_services import get_list_ingredients
+from ..spoonacular_services import get_list_ingredients,\
+                                   get_ingredient_by_id
 
 router = APIRouter(
     prefix='/ingredients',
@@ -31,3 +32,32 @@ def get_ingredients(key_word: str):
     :return: List found ingredients
     """
     return get_list_ingredients(key_word)
+
+
+@router.get("/{ingredient_id}")
+def get_ingredient_info(ingredient_id: int):
+    """
+    Returns Json of ingredient with inforamtion:
+    `{
+        "id": 9266,
+        "name": "pineapples",
+        "possibleUnits": [
+                          "piece",
+                          "slice",
+                          "fruit",
+                          "g",
+                          "oz",
+                          "cup",
+                          "serving"
+        ],
+        "categoryPath": [
+                        "tropical fruit",
+                        "fruit"
+        ],
+        "image": "https://spoonacular.com/cdn/ingredients_500x500/pineapple.jpg"
+    }`
+    \f
+    :param ingredient_id: Ingredient id
+    :return: Json with detail info of our ingredient
+    """
+    return get_ingredient_by_id(ingredient_id)
