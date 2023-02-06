1033ca865f95a8e82bbfea0c79f7822f54eff05b
ivan_masiuk <vavolan@gmail.com>
2023-02-06 19:46:48 +0200

    fix: fix config file

--- server/config.py
+++ server/config.py
@@ -6,8 +6,13 @@ from dotenv import load_dotenv
 from loguru import logger
 
 
-if not load_dotenv():
+if not load_dotenv("app/.env"):
     logger.critical("Add .env file to the directory")
-
+    if not load_dotenv("server/.env"):
+        logger.critical("Add .env file to the directory 2")
+    else:
+        logger.success("OK 2")
+else:
+    logger.success("OK 1")
 
 SPOONCULAR_KEY = os.getenv("SPOONCULAR_KEY")
